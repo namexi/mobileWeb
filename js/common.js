2a80393ae77de223ad7067856131496abdff449e
韩淅 <406633257@qq.com>
2018-08-04 09:05:47 +0800

    18-解决搜索框在华为浏览器上无法改变背景颜色问题

--- js/common.js
+++ js/common.js
@@ -1,3 +1,27 @@
 /**
  * Created by xi on 2018/7/31.
  */
+
+function My$(){}
+
+My$.prototype = {
+    //获取元素
+    select : function (Selector) {
+        return document.querySelector(Selector);
+    },
+    selects : function(Selector){
+        return document.querySelector(Selector);
+    },
+    $id : function (id) {
+        return document.getElementById(id);
+    },
+    tag : function (tagname){
+        return document.getElementsByTagName(tagname);
+    },
+    //处理兼容性
+    getHeight : function(ele){
+        return ele.offsetHeight? ele.offsetHeight:parseInt(window.getComputedStyle(ele,null).height);
+    }
+};
+
+var my$ = new My$();
--- js/search.js
+++ js/search.js
@@ -4,25 +4,22 @@
 
 
 /*搜索对对象*/
-function Search(boxSelector,bannerSelector){
-    //配置元素信息
-    this.config={
-            search : document.querySelector(boxSelector),
-            banner : document.querySelector(bannerSelector),
-    };
-
+function Search(src,login){
+    //基本信息
+    this.icon = src;
+    this.login = login;
 }
 
 Search.prototype={
     //绑定scroll事件
     scroll : function(obj){
-        var that = this;
+        var opcation = 0;
         obj.onscroll=function(){
             //获取盒子高度
-            var height = that.config.banner.offsetHeight;
+            var height = my$.select('.jd_banner').offsetHeight;
             //获取页面滚动
-            var scrollTOP = document.documentElement.scrollTop;
-            var opcation = 0;
+            var scrollTOP = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+
             if(scrollTOP < height){
                 //页面滚动时，随着页面卷曲高度变大 透明度变大
                 opcation = scrollTOP / height *1;
@@ -30,8 +27,8 @@ Search.prototype={
                 //    页面滚动时 超过某一高度 透明度保持固定值不变
                 opcation = 0.85;
             }
-            that.config.search.style.background = 'rgba(201,21,35,'+opcation+')';
-        }
+            my$.select('.jd_search').style.background = 'rgba(201,21,35,'+opcation+')';
+        };
 
     }
 };
